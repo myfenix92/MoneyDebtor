e88f39fd8df839cd6e254966ce0182ea026fe76c
Yana Korneykova <bloom.hog@gmail.com>
2023-10-08 22:03:49 +0300

    add datepickerdialog, change edit layout

--- app/src/main/java/com/hfad/moneydebtor/DetailActivity.java
+++ app/src/main/java/com/hfad/moneydebtor/DetailActivity.java
@@ -2,7 +2,9 @@ package com.hfad.moneydebtor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+    }
+
+    public void addNewUser(View view) {
+        Intent intent = new Intent(this, EditActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/hfad/moneydebtor/EditActivity.java
+++ app/src/main/java/com/hfad/moneydebtor/EditActivity.java
@@ -2,13 +2,80 @@ package com.hfad.moneydebtor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class EditActivity extends AppCompatActivity {
-
+    final Calendar myCalendar = Calendar.getInstance();
+    EditText dateTake;
+    EditText dateGive;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        dateTake = (EditText) findViewById(R.id.edit_date_take);
+        dateGive = (EditText) findViewById(R.id.edit_date_give);
+        DatePickerDialog.OnDateSetListener dateTakeDialog = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                setDateTake();
+            }
+        };
+
+        DatePickerDialog.OnDateSetListener dateGiveDialog = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                setDateGive();
+            }
+        };
+
+        dateTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(EditActivity.this, dateTakeDialog, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        dateGive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(EditActivity.this, dateGiveDialog, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+    }
+
+    private void setDateTake(){
+        String myFormat="dd.MM.yyyy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        dateTake.setText(dateFormat.format(myCalendar.getTime()));
+    }
+
+    private void setDateGive(){
+        String myFormat="dd.MM.yyyy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        dateGive.setText(dateFormat.format(myCalendar.getTime()));
+    }
+
+    public void addNewRecord(View view) {
+        Toast.makeText(this, dateTake.getText(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, dateGive.getText(), Toast.LENGTH_SHORT).show();
     }
 }
