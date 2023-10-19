ca4b8846e184e5912cff774f3bf7bae676d0a19a
Yana Korneykova <bloom.hog@gmail.com>
2023-10-19 15:15:55 +0300

    feat: add en, uk lang, all count summa in menu, apk file

--- app/src/main/java/com/hfad/moneydebtor/MainActivity.java
+++ app/src/main/java/com/hfad/moneydebtor/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -169,6 +170,10 @@ public class MainActivity extends AppCompatActivity {
             CoordinatorLayout coordinatorLayoutDetail = findViewById(R.id.coordinator_main);
             aboutHelper(coordinatorLayoutDetail);
         }
+        if (menuItem.getItemId() == R.id.menu_all_summa) {
+            CoordinatorLayout coordinatorLayoutDetail = findViewById(R.id.coordinator_main);
+            countAllSummaDialog(coordinatorLayoutDetail);
+        }
         saveText(menuItem.getItemId(), isView);
         return super.onOptionsItemSelected(menuItem);
     }
@@ -179,6 +184,40 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setIcon(android.R.drawable.sym_action_chat);
         builder.setTitle(R.string.about_title);
+        builder.setView(promtView);
+        builder.setPositiveButton(R.string.ok_btn, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    public void countAllSumma(View view) {
+        double allSummaRed = 0.0;
+        double allSummaGreen = 0.0;
+        for (int i = 0; i < usersDatasetList.size(); i++) {
+            if (usersDatasetList.get(i).getAll_summa() > 0) {
+                allSummaRed += usersDatasetList.get(i).getAll_summa();
+            } else {
+                allSummaGreen += usersDatasetList.get(i).getAll_summa();
+            }
+        }
+        TextView allRedText = view.findViewById(R.id.all_summa_red);
+        allRedText.setText(String.valueOf(allSummaRed));
+        TextView allGreenText = view.findViewById(R.id.all_summa_green);
+        allGreenText.setText(String.valueOf(allSummaGreen));
+    }
+
+    public void countAllSummaDialog(View view) {
+        LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+        View promtView = layoutInflater.inflate(R.layout.alert_all_summa, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(android.R.drawable.stat_notify_more);
+        builder.setTitle(R.string.menu_all_summa);
+        countAllSumma(promtView);
         builder.setView(promtView);
         builder.setPositiveButton(R.string.ok_btn, new DialogInterface.OnClickListener() {
             @Override
